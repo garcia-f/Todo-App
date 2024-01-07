fdbf1a015829a79a739ba0afd415ed93a69f51ea
Fabian <garciafabianemmanuel@gmail.com>
2024-01-07 20:11:05 -0300

    creacion de funciones en todo.store

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -25,7 +25,73 @@ const initStore = () => {
 
 }
 
+const loadStore = () => {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Agregar un nuevo Todo 
+ * @param {String} description 
+ */
+const addTodo = ( description ) => {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Actualizacion: para saber si esta terminado o no 
+ * @param {String} todoId 
+ */
+const toggleTodo = ( todoId ) => {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Eliminar un Todo
+ * @param {String} todoId 
+ */
+const deleteTodo = ( todoId ) => {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Para eliminar los Todo completados
+ */
+const deletedCompleted = () => {
+  throw new Error('NOt implemented');
+}
+
+/**
+ * Para seleccionar un filtro
+ * @param {Object} newFilter 
+ */
+const setFilter = ( newFilter = Filters.All ) => {
+  throw new Error('NOt implemented');  
+}
+
+/**
+ * Para saber cual es el filtro seleccionado
+ */
+const getCurrentFilter = () => {
+  throw new Error('NOt implemented');
+}
+
+
 
 export default {
+  addTodo,
+  deletedCompleted,
+  deleteTodo,
+  getCurrentFilter,
   initStore,
+  loadStore,
+  setFilter,
+  toggleTodo,
 }
+
+
+
+
+
+
+
+
